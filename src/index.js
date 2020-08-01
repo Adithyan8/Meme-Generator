34fad0cb6a72a0832cee514fdbf2ea6ea3a91c31
Adithyan8 <t.n.adithyan@hotmail.com>
2020-08-01 16:24:14 +0530

    memegen

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import MyApp from './components/MyApp';
+
+
+ReactDOM.render(
+  <MyApp/>,
+  document.getElementById('root')
+);
+
